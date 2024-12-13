9b5b0de64ee022d2a7f8cf36138ffa8472cf1818
Pablo Baxter <pablobaxter@msn.com>
2024-12-12 22:21:53 -0800

    Initial commit

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,18 @@
+//plugins {
+//    kotlin("jvm") version "2.0.20"
+//}
+
+plugins {
+    `kotlin-dsl`
+    alias(libs.plugins.kotlin.jvm)
+}
+
+dependencies {
+    implementation(gradleApi())
+    implementation(gradleKotlinDsl())
+    implementation(libs.kotlin.gradle.plugin)
+    implementation(libs.kotlin.dokka.plugin)
+    implementation(libs.ktlint.plugin)
+    implementation(libs.dependency.analysis)
+    implementation(libs.vanniktech)
+}
---
+++ buildSrc/src/main/kotlin/com/frybits/plugins/PluginUtils.kt
@@ -0,0 +1,52 @@
+/*
+ *  Copyright 2024 Pablo Baxter
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ * Created by Pablo Baxter (Github: pablobaxter)
+ */
+
+package com.frybits.plugins
+
+import com.autonomousapps.DependencyAnalysisPlugin
+import com.autonomousapps.DependencyAnalysisSubExtension
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.apply
+import org.gradle.kotlin.dsl.configure
+import org.jlleitschuh.gradle.ktlint.KtlintExtension
+import org.jlleitschuh.gradle.ktlint.KtlintPlugin
+
+internal fun Project.configureCommon() {
+    applyKtlint()
+    applyDependencyAnalysis()
+}
+
+internal fun Project.applyKtlint() {
+    apply<KtlintPlugin>()
+    configure<KtlintExtension> {
+        version.set("0.48.2")
+        outputToConsole.set(true)
+        outputColorName.set("RED")
+    }
+}
+
+internal fun Project.applyDependencyAnalysis() {
+    apply<DependencyAnalysisPlugin>()
+    configure<DependencyAnalysisSubExtension> {
+        issues {
+            onAny {
+                severity("fail")
+            }
+        }
+    }
+}
